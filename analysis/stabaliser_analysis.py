86418db694efc8bfc2517c0e8e3e7e8762d6465c
EdLeming <e.leming09@googlemail.com>
2017-02-05 22:16:57 +0000

    Updated read string to handle more standadised format

--- analysis/stabaliser_analysis.py
+++ analysis/stabaliser_analysis.py
@@ -17,7 +17,7 @@ import utils.utils as utils
 import numpy as np
 from collections import defaultdict
 
-colors = [ROOT.kBlack, ROOT.kRed-4, ROOT.kOrange+7, ROOT.kGreen+2, ROOT.kCyan+2, ROOT.kBlue, ROOT.kMagenta+1]
+colors = [ROOT.kBlack, ROOT.kRed-4, ROOT.kOrange+7, ROOT.kGreen+2, ROOT.kCyan+2, ROOT.kBlue, ROOT.kMagenta+1, ROOT.kGray]
 markers = [4,5,2,7]
 
 class DataStructure(object):
@@ -25,31 +25,56 @@ class DataStructure(object):
     def __init__(self, hist_name, histo):
         """Init structure"""
         self._histo = histo
+        self._hist_name = hist_name
         self._relative_time = None
         self.unpack_hist_name()
         self.estimate_end_point()
 
     def unpack_hist_name(self):
         """Read some interesting parameters from hist name string"""
-        # Unpack date
-        datestr = "-".join(re.findall(r'\d+', hist_name[:8]))
+        variables = self._hist_name.split("_")
+        blank = False
+        pure_lab = False
+        if len(variables) == 5:
+            blank = False
+        elif len(variables) == 4:
+            blank  = True
+        elif len(variables) == 2:
+            pure_lab = True
+        else:
+            print "Looks like the file name doesn't follow convention:"
+            print self._hist_name
+            print "It should look like: date_Te%_PPO_ratio_A/B"
+            print "If you've added an extra descriptor please remove and re-try"
+            sys.exit(1)
+        ##################
+        # Unpack variables from string
+        # Date
         try:
-            date = datetime.datetime.strptime(datestr, "%d-%m-%y")
+            date = datetime.datetime.strptime(variables[0], "%d%m%y")
         except:
             try:
-                date = datetime.datetime.strptime(datestr, "%d-%m-%Y")
+                date = datetime.datetime.strptime(variable[0], "%d%m%Y")
             except:
                 print "Looks like there isn't a date at the start of the histo name..."
                 raise
         self._date = date
-
-        # Unpack concentration and sample label
-        details = hist_name.split("_")[-1]
-        concentration = ".".join(re.findall(r'\d+', details))
-        if concentration == "":
-            concentration = "0.0"
+        
+        # Molar ratio (concentration)
+        if blank:
+            concentration = "0.0"            
+        elif pure_lab:
+            concentration = ""
+        else:
+            concentration_field = variables[-2]
+            concentration = ".".join(re.findall(r'\d+', concentration_field))
         self._conc = concentration
-        self._sample = details[-1]
+
+        # Sample name
+        if pure_lab:
+            self._sample = "Pure_LAB"
+        else:
+            self._sample = variables[-1]
         self._identifier = "{0}{1}".format(self._conc, self._sample)
 
     def estimate_end_point(self):
@@ -59,11 +84,12 @@ class DataStructure(object):
         current_bin = self._histo.FindLastBinAbove(0)
         sum = 0
         x,y = [],[]
-        while(sum<70):
+        while(sum<75):
             content = self._histo.GetBinContent(current_bin)
-            sum = sum + content
             current_bin = current_bin - 1
             if content > 1:
+                sum = sum + content
+                self._histo.GetBinLowEdge(current_bin), content, sum
                 x.append(self._histo.GetBinLowEdge(current_bin))
                 y.append(content) 
         self._endpoint_median = ROOT.TMath.Median(len(x), np.array(x), np.array(y))
@@ -83,6 +109,9 @@ class DataStructure(object):
 
     def get_histo(self):
         return self._histo
+
+    def get_hist_name(self):
+        return self._hist_name
 
     def get_endpoint_median(self):
         return self._endpoint_median
@@ -150,7 +179,8 @@ if __name__ == "__main__":
     # Add a relative time field to the objects
     for data in data_array:
         data.set_relative_time( data.get_date()-check )
-
+    # Sort by date for nice plotting
+    data_array.sort(key=lambda x: x.get_date())
 
     #######################
     # Make output file and put the raw
@@ -159,9 +189,10 @@ if __name__ == "__main__":
     out_dir = utils.check_dir(args.out_dir)
     outFile = ROOT.TFile("{0}stabaliser_plots.root".format(out_dir), "recreate")
 
-    for dKey in dates:
+    for dKey in sorted(dates):
         for sKey in samples:
             # Make title strings and root objects for this loop
+            hist = False
             date_str = dKey.strftime("%d/%m/%Y")
             base_name = "{0}_{1}".format(date_str, sKey)
             stack_name = "THStack_{0}".format(base_name)
@@ -179,26 +210,32 @@ if __name__ == "__main__":
                     hist = data.get_histo()
                     hist.SetLineColor(colors[counter])
                     stack.Add(hist)
-                    legend_str = "{0} {1}:Te".format(data.get_concentration(), args.stabaliser)
+                    if data.get_concentration == "":
+                        legend_str = "LAB {0}g/l PPO".format(args.ppo)
+                    else:
+                        legend_str = "{0} {1}:Te".format(data.get_concentration(), args.stabaliser)
                     legend.AddEntry(data.get_histo(), legend_str)
                     counter = counter + 1
 
-            # Formatting
-            stack.SetTitle(stack_title)
-            stack.Draw("nostack")
-            stack.GetXaxis().SetTitle("ADC")
-            stack.GetYaxis().SetTitle("Counts / ADC bin")
-            stack.SetMinimum(1)
-            max_x = round(stack.GetStack().Last().FindLastBinAbove(1)/10.)*10.
-            stack.GetXaxis().SetRangeUser(0, max_x)
-            can.SetLogy()
-            
-            # Draw and write
-            stack.Write()
-            legend.Draw()
-            can.Update()
-            can.Write()
-            del can
+            # Possible some measurements were not taken on all dates
+            if hist:
+                # Formatting
+                stack.SetTitle(stack_title)
+                stack.Draw("nostack")
+                stack.GetXaxis().SetTitle("ADC")
+                stack.GetYaxis().SetTitle("Counts / ADC bin")
+                stack.SetMinimum(1)
+                max_x = round(stack.GetStack().Last().FindLastBinAbove(1)/10.)*10.
+                stack.GetXaxis().SetRangeUser(0, max_x)
+                can.SetLogy()
+                
+                # Draw and write
+                stack.Write()
+                legend.Draw()
+                can.Update()
+                can.Write()
+                can.SaveAs("./results/Spectra_{0}_{1}.png".format(sKey, dKey.strftime("%d%m%y")))
+                del can
 
     ########################
     # Make endpoint vs date plot
@@ -206,9 +243,9 @@ if __name__ == "__main__":
     # First make TGraph for each sample concentration
     #
     TGraphs = {}
-    graph_legend = ROOT.TLegend(0.65,0.4,0.9,0.9);
+    graph_legend = ROOT.TLegend(0.80,0.4,0.995,0.9);
     graph_legend.SetFillStyle(0)
-    for iKey in ids:
+    for iKey in sorted(ids):
         graph = ROOT.TGraph()
         # Set marker style by keys
         for i, sKey in enumerate(samples):
@@ -218,16 +255,20 @@ if __name__ == "__main__":
         for i, cKey in enumerate(concentrations):
             if ids[iKey][0].get_concentration() == cKey:
                 graph.SetLineColor(colors[i])
+                graph.SetMarkerColor(colors[i])
                 graph.SetFillStyle(0)
                 graph.SetFillColor(0)
-                graph.SetMarkerColor(colors[i])
-        legend_str = "{0} molar ratio {1}".format(ids[iKey][0].get_concentration(), ids[iKey][0].get_sample())
+        if ids[iKey][0].get_concentration() == "":
+            legend_str = "Pure LAB-PPO".format(ids[iKey][0].get_concentration(), ids[iKey][0].get_sample())
+        else:
+            legend_str = "{0} molar ratio {1}".format(ids[iKey][0].get_concentration(), ids[iKey][0].get_sample())
         graph_legend.AddEntry(graph, legend_str)
         TGraphs[iKey] = graph
 
     ###############
     # Fill those graphs
     multiCan = ROOT.TCanvas("TCanvas_Enpoint_vs_date", "multi", 1)
+    multiCan.SetRightMargin(0.20);
     for data in data_array:
         this_id =  data.get_id()
         points = TGraphs[this_id].GetN()
@@ -236,13 +277,15 @@ if __name__ == "__main__":
     ################
     # Put filled graphs into to a multigraph and draw
     multiGraph = ROOT.TMultiGraph()
-    for key in TGraphs:
+
+    for key in sorted(TGraphs):
         multiGraph.Add(TGraphs[key])
-    multiGraph.Draw("ap")
+    multiGraph.Draw("apl")
     multiGraph.GetXaxis().SetTitle("Time [days]")
-    multiGraph.GetYaxis().SetTitle("Endpoint [ADC]")
+    multiGraph.GetYaxis().SetTitle("Estimated Endpoint [ADC units]")
     multiGraph.SetName("TMulti_Endpoints_vs_time")
     graph_legend.Draw()
     multiGraph.Write()
     multiCan.Update()
     multiCan.Write()
+    multiCan.SaveAs("./results/Endpoints.png")
