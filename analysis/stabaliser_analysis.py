220361e387f63e413ccf7e1a5f81899c1e2f1a23
Tereza Kroupa <tereza.kroupova@physics.ox.ac.uk>
2017-02-23 13:30:22 +0000

    Add normalised endpoint vs time plot
    - add new TMultiGraph to the output file of stabaliser_analysis
    - normalise endpoints by the offset of LAB measurement from average
    - change median to mean in get_endpoint_mean method
    - remove formatter on line 265 of stabaliser_analysis.py

--- analysis/stabaliser_analysis.py
+++ analysis/stabaliser_analysis.py
@@ -120,10 +120,10 @@ class DataStructure(object):
 
     def get_endpoint_mean(self):
         try:
-            return self._endpoint_median
+            return self._endpoint_mean
         except:
-            _, median = self.estimate_end_point()
-            return median
+            _, mean = self.estimate_end_point()
+            return mean
 
     def get_relative_time(self, units="days"):
         if units == "days":
@@ -262,7 +262,7 @@ if __name__ == "__main__":
                 graph.SetFillStyle(0)
                 graph.SetFillColor(0)
         if ids[iKey][0].get_concentration() == "":
-            legend_str = "Pure LAB-PPO".format(ids[iKey][0].get_concentration(), ids[iKey][0].get_sample())
+            legend_str = "Pure LAB-PPO"
         else:
             legend_str = "{0} molar ratio {1}".format(ids[iKey][0].get_concentration(), ids[iKey][0].get_sample())
         graph_legend.AddEntry(graph, legend_str)
@@ -276,6 +276,7 @@ if __name__ == "__main__":
         this_id =  data.get_id()
         points = TGraphs[this_id].GetN()
         TGraphs[this_id].SetPoint(points, data.get_relative_time(), data.get_endpoint_median())
+
 
     ################
     # Put filled graphs into to a multigraph and draw
@@ -292,3 +293,55 @@ if __name__ == "__main__":
     multiCan.Update()
     multiCan.Write()
     multiCan.SaveAs("./results/Endpoints.png")
+
+    
+    ################
+    # Create normalised endpoint vs time plot
+    #
+    # Make a dictionary of relative offsets of LAB from its average
+    lab_offset={}
+    for iKey in sorted(ids): 
+        if ids[iKey][0].get_concentration() == "":
+            average_lab=TGraphs[iKey].GetMean(2)
+            for data in ids[iKey]:
+                lab_offset[data.get_relative_time()] = (average_lab - data.get_endpoint_median())/average_lab
+            break
+
+    ###############
+    # Normalise graphs using LAB offset
+    TGraphsNormalised={}
+    point_x, old_y, new_y = ROOT.Double(0.0), ROOT.Double(0.0),ROOT.Double(0.0)
+    for iKey in sorted(ids):
+        if ids[iKey][0].get_concentration() != "":
+            TGraphsNormalised[iKey]=TGraphs[iKey].Clone()
+            points = TGraphsNormalised[iKey].GetN()
+            for point, data in enumerate(sorted(ids[iKey])):
+                TGraphsNormalised[iKey].GetPoint(point, point_x, old_y)
+                if point_x in lab_offset:
+                    new_y = old_y*(1+lab_offset[point_x])
+                    TGraphsNormalised[iKey].SetPoint(point, point_x, new_y)
+                else:
+                    print "No value for pure LAB, cannot normalise."
+        
+    #################
+    # Create multigraph for normalised endpoint vs time plots
+    multiGraphNorm = ROOT.TMultiGraph()
+    multiCanNorm = ROOT.TCanvas("TCanvas_Enpoint_vs_date_normalised", "multiNorm", 1)
+    multiCanNorm.SetRightMargin(0.20);
+    for key in sorted(TGraphsNormalised):
+        multiGraphNorm.Add(TGraphsNormalised[key])
+
+    # Delete pure LAB from the legend
+    graph_legend.GetListOfPrimitives().Last()
+    graph_legend.DeleteEntry()                
+
+    # Format and print
+    multiGraphNorm.Draw("apl")
+    multiGraphNorm.GetXaxis().SetTitle("Time [days]")
+    multiGraphNorm.GetYaxis().SetTitle("Estimated Endpoint [ADC units]")
+    multiGraphNorm.SetName("TMulti_Endpoints_vs_time")
+    graph_legend.Draw()
+    multiGraphNorm.Write()
+    multiCanNorm.Update()
+    multiCanNorm.Write()
+    multiCanNorm.SaveAs("./results/EndpointsNorm.png")
